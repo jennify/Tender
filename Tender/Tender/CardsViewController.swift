55c0e884276aa780d70b3093559822d4b371e598
Jennifer Lee <jlee@dropbox.com>
2016-03-02 19:37:28 -0800

    Swipe done

---
+++ Tender/Tender/CardsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Tender
+//
+//  Created by Jennifer Lee on 3/2/16.
+//
+//
+
+import UIKit
+
+class CardsViewController: UIViewController {
+    var originalCenter: CGPoint!
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+//        let gesture = UIGestureRecognizer(target: self, action: "onGesture:")
+//        self.profileImageView.addGestureRecognizer(gesture)
+        self.profileImageView.userInteractionEnabled = true
+
+    }
+
+    @IBAction func profileImagePanGesture(sender: UIPanGestureRecognizer) {
+        
+        if sender.state == UIGestureRecognizerState.Began {
+            self.originalCenter = self.profileImageView.center
+        } else if sender.state == UIGestureRecognizerState.Changed {
+            self.profileImageView.center = CGPoint(x: self.originalCenter.x + sender.translationInView(view).x, y: originalCenter.y)
+        } else if sender.state == UIGestureRecognizerState.Ended {
+            
+        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
